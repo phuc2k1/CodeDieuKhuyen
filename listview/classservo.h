62b40e7e288f5971e5e425e95ac859904e531d57
phuc2k1 <126533850+phuc2k1@users.noreply.github.com>
2023-04-18 09:49:52 +0700

    Update classservo.h

--- listview/classservo.h
+++ listview/classservo.h
@@ -1,15 +1,22 @@
 #ifndef CLASSSERVO_H
 #define CLASSSERVO_H
-
-
+#include <wiringPi.h>
+#include <softPwm.h>
+#include "qdebug.h"
+#include"stdint.h"
 class classservo
 {
 public:
-    classservo();
+
+    classservo(int pin);
     void setangle(int angle );
     int getangle();
+    int getpinmode();
+    void setpinmode(int a);
+
 private:
-    int angle;
+    int localangle;
+    int SERVO_PIN;
 };
 
 #endif // CLASSSERVO_H
