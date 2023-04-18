1fa8f3a7a799ee862d069b85fee0ca55b4235f6e
phuc2k1 <126533850+phuc2k1@users.noreply.github.com>
2023-04-18 09:48:44 +0700

    Update classservo.cpp

--- listview/classservo.cpp
+++ listview/classservo.cpp
@@ -1,7 +1,14 @@
 #include "classservo.h"
-#include"stdint.h"
-classservo::classservo()
+
+
+classservo::classservo(int a)
 {
+    setpinmode(a);
+    wiringPiSetup();
+    pinMode(SERVO_PIN,OUTPUT);
+    softPwmCreate(SERVO_PIN,0,50);
+    qDebug()<<"Success INIT Servo:"<<a;
+
 
 }
 
@@ -10,10 +17,29 @@ void classservo::setangle(int angle)
     if(angle<0){angle=0;}
     if(angle>180){angle=180;}
     uint32_t intension=5+ (angle/9);
-    //them
+    qDebug()<<SERVO_PIN<<"="<<angle;
+    if(localangle!=angle)
+        {
+        softPwmWrite (SERVO_PIN, intension);
+        localangle=angle;
+        int time=std::abs(localangle-angle)+20;
+        delay(time);
+        }
+
+
 }
 int classservo::getangle(){
-    return angle;
+    return localangle;
 }
+
+int classservo::getpinmode()
+{
+    return SERVO_PIN;
+}
+void classservo::setpinmode(int a){
+    SERVO_PIN=a;
+}
+
+
 
 
